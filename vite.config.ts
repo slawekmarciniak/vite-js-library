5c78991e0b47450e49c0fd143e55203d2696e52f
Sławomir Marciniak <slawomir.marciniak@getresponse.com>
2023-03-02 12:26:03 +0100

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,22 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'MyLib',
-      // the proper extensions will be added
-      fileName: 'main',
-      formats: ['cjs', 'es']
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        // entryFileNames: `[name].[hash].mjs`,
-        // chunkFileNames: `[name].[hash].mjs`,
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          'import.meta.env.DEV': 'true',
-          'import.meta.env.PROD': 'false',
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'MyLib',
+            fileName: 'main',
+            formats: ['cjs', 'es']
         },
-      },
+        rollupOptions: {
+            external: [],
+            output: {
+                globals: {
+                },
+            },
+        },
     },
-  },
-  plugins: [dts({
-    insertTypesEntry: true,
-  })]
-})
+    plugins: [dts({
+        insertTypesEntry: true,
+    })],
+});
